801420851f8a3aa35f0bafc4215157cd164506a1
Your Name <578180583@qq.com>
2019-08-20 14:16:21 +0800

    add photos

--- tool.py
+++ tool.py
@@ -130,7 +130,7 @@ def handle_photo():
     list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
 
-    with open("C:/Users/ZXH/hexo/source/photos/data.json","w") as fp:
+    with open("C:\\Users\\ZXH\\hexo\\source\\photos\\data.json","w") as fp:
         json.dump(final_dict, fp)
 
 def cut_photo():
